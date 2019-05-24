f8e388d385305673572b38848cee0c1ad91bc603
Victor Martins <victormartins@konstroi.com>
2019-05-23 21:05:10 -0300

    first commit

---
+++ dist/out-tsc/src/app/app.component.js
@@ -0,0 +1,32 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+var AppComponent = /** @class */ (function () {
+    function AppComponent(platform, splashScreen, statusBar) {
+        this.platform = platform;
+        this.splashScreen = splashScreen;
+        this.statusBar = statusBar;
+        this.initializeApp();
+    }
+    AppComponent.prototype.initializeApp = function () {
+        var _this = this;
+        this.platform.ready().then(function () {
+            _this.statusBar.styleDefault();
+            _this.splashScreen.hide();
+        });
+    };
+    AppComponent = tslib_1.__decorate([
+        Component({
+            selector: 'app-root',
+            templateUrl: 'app.component.html'
+        }),
+        tslib_1.__metadata("design:paramtypes", [Platform,
+            SplashScreen,
+            StatusBar])
+    ], AppComponent);
+    return AppComponent;
+}());
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
---
+++ dist/out-tsc/src/app/home/home.module.js
@@ -0,0 +1,30 @@
+import * as tslib_1 from "tslib";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HomePage } from './home.page';
+var HomePageModule = /** @class */ (function () {
+    function HomePageModule() {
+    }
+    HomePageModule = tslib_1.__decorate([
+        NgModule({
+            imports: [
+                CommonModule,
+                FormsModule,
+                IonicModule,
+                RouterModule.forChild([
+                    {
+                        path: '',
+                        component: HomePage
+                    }
+                ])
+            ],
+            declarations: [HomePage]
+        })
+    ], HomePageModule);
+    return HomePageModule;
+}());
+export { HomePageModule };
+//# sourceMappingURL=home.module.js.map
